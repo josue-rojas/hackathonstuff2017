ed4f243ee50ee0e01df55d9bbaaceb7efbefdad0
timashana <31852525+timashana@users.noreply.github.com>
2017-11-11 04:22:33 -0500

    Add files via upload

--- webcam.py
+++ webcam.py
@@ -14,7 +14,7 @@ def main():
     FaceDetector= cv2.CascadeClassifier("D:/Hackathon/opencv-3.1.0/etc/haarcascades/haarcascade_frontalface_alt.xml")
     EyeDetector = cv2.CascadeClassifier("D:/Hackathon/opencv-3.1.0/etc/haarcascades/haarcascade_eye.xml")
     file = "test_image.png"
-    
+    color = 1
     while(True):
         ret, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -24,6 +24,7 @@ def main():
             cv2.rectangle(img,(x,y),(x1,y1),(255,0,0),2)
             cv2.line(img,(int(x+(x1-x)/2),y),(int(x+(x1-x)/2),y1),(255,0,0),2)
             cv2.line(img,(x,int(y+(y1-y)/2)),(x1,int(y+(y1-y)/2)),(255,0,0),2)
+            color = img [int(x+(x-x1)/2 -6), int(y+(y-y1)/2 -6)]
             croppedImage = img[y:y1, x:x1]
             eyes = EyeDetector.detectMultiScale(gray, 1.3, 5)
             for (ex,ey,ew,eh) in eyes:
@@ -33,12 +34,11 @@ def main():
                 
             cv2.imshow("cropped.png", croppedImage)
             cv2.imwrite(file,croppedImage)
-            
-        
+        print(color)    
         cv2.imshow('frame',img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break;
-        
+            
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     cap.release()
